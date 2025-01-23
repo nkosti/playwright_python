f51a821d26a478a294626619d638656fb6bceb4a
nkosti <barzimon@gmail.com>
2025-01-23 23:04:23 +0100

    Add constants file.

---
+++ test/constants.py
@@ -0,0 +1,11 @@
+"""
+This module contains constants that are used throughout the project.
+These constants should not be modified at runtime.
+
+Constants:
+- `DOWNLOADED_FOLDER`: The default folder where downloaded files are stored.
+
+"""
+
+# File/Folder Constants
+DOWNLOADED_FOLDER = "downloads"
