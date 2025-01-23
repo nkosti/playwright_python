f1d544f97f6e36cb53f641f44729819aeb60c39d
nkosti <barzimon@gmail.com>
2025-01-23 23:08:18 +0100

    Add base_pom.

--- test/domains/page_objects/base_pom/base_pom.py
+++ test/domains/page_objects/base_pom/base_pom.py
@@ -1,3 +1,5 @@
+import logging
+
 from playwright.sync_api import Page
 
 from test import constants
@@ -8,6 +10,8 @@ class BasePOM:
     def __init__(self, page: Page):
         self.page = page
         self.loc = Locators
+
+    LOGGER = logging.getLogger(__name__)
 
     def download_file(
             self,
@@ -34,3 +38,34 @@ class BasePOM:
         download.save_as(dir_to_save)
 
         return dir_to_save
+
+    def return_successful_notification_element(self):
+        return self.page.locator(self.loc.SUCCESSFUL_NOTIFICATION).last
+
+    def waiting_for_spinner(
+            self, timeout=3, spinner_element=Locators.SPINNER_ELEMENT
+    ):
+        """
+        Waits for the loading spinner to appear and disappear.
+        If the spinner does not appear, the method continues without failing.
+
+        :param poms: Context object that acts as a Page Factory and context manager.
+        :param timeout: Maximum time to wait for the spinner to appear, in seconds.
+        :param spinner_element: Spinner element can be provided if different from the base element.
+        """
+
+        try:
+            # Wait for the spinner to appear with a timeout
+            # To appear normally will not want more than 3 seconds
+            self.page.locator(spinner_element).wait_for(
+                state="visible", timeout=timeout * 1000
+            )
+            # If spinner appears, wait for it to disappear
+            # The spinner could take longer to disappear so removing the timeout and implicitly take the default 30 sec
+            self.page.locator(spinner_element).wait_for(state="hidden")
+        except Exception as e:
+            self.LOGGER.info(
+                f"Exception throw in waiting_for_loading_the_result function"
+            )
+            # If spinner does not appear, or any other exception occurs, continue without failing
+            pass
